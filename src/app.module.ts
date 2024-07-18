92ab09f0bc30924e639b667b4b80bc3a74d9f1cb
Andres Fariña <farinavaldivia@gmail.com>
2024-07-18 19:11:16 -0400

    segundo commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EmpresaModule } from './empresa/empresa.module';
+import { EstudianteModule } from './estudiante/estudiante.module';
+import { OfertaLaboralModule } from './oferta-laboral/oferta-laboral.module';
+import { PostulacionModule } from './postulacion/postulacion.module';
 
 @Module({
-  imports: [],
+  imports: [EmpresaModule, EstudianteModule, OfertaLaboralModule, PostulacionModule],
   controllers: [AppController],
   providers: [AppService],
 })
