a8c1c3fc731071b9cf096b5fc95a3193c94b8fd0
Andres Fariña <farinavaldivia@gmail.com>
2024-07-18 20:04:26 -0400

    NPM No Corre

--- src/estudiante/estudiante.service.ts
+++ src/estudiante/estudiante.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { Estudiante } from './estudiante';
 
 @Injectable()
-export class EstudianteService {}
+export class EstudianteService {
+    private estudiantes: Estudiante[] = [];
+
+  create(estudiante: Estudiante): Estudiante {
+    this.estudiantes.push(estudiante);
+    return estudiante;
+  }
+
+  findAll(): Estudiante[] {
+    return this.estudiantes;
+  }
+
+  findOne(id: number): Estudiante {
+    return this.estudiantes.find(estudiante => estudiante.id === id);
+  }
+
+  remove(id: number): void {
+    this.estudiantes = this.estudiantes.filter(estudiante => estudiante.id !== id);
+  }
+}
+npm
